4bc9a3e80cbed6067c047d8dbdd9c15bcf449516
nupur29 <patenupu@sheridancollege.ca>
2022-02-22 14:19:16 -0500

    Added a new TestUserProfile File

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,19 @@
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author NUPUR
+ */
+public class TestUserProfile {
+    
+    public static void main(String[] args){
+    
+        UserProfile profile = new UserProfile();
+        
+        userID.setUserID(2);
+        genres.setGenre("Comedy", "Action");
+        
+        System.out.println("Profile created");
+    }
+    
+}
